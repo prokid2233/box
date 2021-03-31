ae99fa2a13c6c10a88fce6c75ac6550426431353
prokid2233 <75042573+prokid2233@users.noreply.github.com>
2021-03-31 16:14:09 -0400

    Add files via upload

---
+++ TowerSiege-1-main/Block.js
@@ -0,0 +1,19 @@
+class Block extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/wood1.png");
+    }
+  
+  };
+ 
+  block8= new Block(330,235,30,40)
+  block9= new Block(360,235,30,40)
+  block10= new Block(390,235,30,40)
+  block11= new Block(420,235,30,40)
+  block12= new Block(450,235,30,40)
+  //3
+  block13= new Block(360,195,30,40)
+  block14= new Block(390,195,30,40)
+  block15= new Block(420,195,30,40)
+  //top
+  block16= new Block(390,155,30,40)
